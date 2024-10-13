3b173aa663c3b36aa7d34c5e22ea3de6ecee1f91
Anuprita2022-26 <2022.anuprita.mhapankar@ves.ac.in>
2024-10-13 22:42:47 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="p-4 bg-yellow-600 dark:bg-gray-800 text-white text-center bottom-0 sticky w-full">
+      <p>&copy; 2024 Anuprita Mhapankar - Chennai Super Kings Fan</p>
+    </footer>
+  );
+};
+
+export default Footer;
