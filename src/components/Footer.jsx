b15878411dde1422f5d3b60270b7442528045461
Anuprita2022-26 <2022.anuprita.mhapankar@ves.ac.in>
2024-10-13 22:53:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="p-4 bg-yellow-600 dark:bg-gray-800 text-white text-center bottom-0 sticky w-full">
-      <p>&copy; 2024 Anuprita Mhapankar - Chennai Super Kings Fan</p>
+      <p>&copy; 2024 Anuprita Mhapankar </p>
     </footer>
   );
 };
